9b26a9f06420d62a66701d877f51208e53ffc81d
Boseung Choi <cbskust@gmail.com>
2017-11-26 14:16:20 +0000

    version 0.1.0

---
+++ R/Chymo_high.R
@@ -0,0 +1,12 @@
+#' Product concentration of 101 observed time
+#'
+#' An aritficial data set containing the product concentration
+#' and it's observed time based on high enzyme concentration and
+#' low sustrate concentration
+#'
+#' @format A data frame with 101 rows and 2 variables:
+#' \describe{
+#' \item{time}{observed time, no unit}
+#' \item{product}{product concentration, no unit}
+#' }
+"Chymo_high"
